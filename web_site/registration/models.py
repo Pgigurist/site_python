b8fa23df3b0e336c3bde2220901ad21d6d541c1d
Alexey Gubanov <pgigurist@gmail.com>
2019-03-22 21:07:41 +0300

    create MasterClass and Entry models

--- web_site/registration/models.py
+++ web_site/registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class MasterClass(models.Model):
+    name = models.CharField(max_length=100)
+    date_start = models.DateTimeField('date start')
+    date_end = models.DateTimeField('date end')
+
+class Entry(models.Model):
+    user_id     = models.IntegerField(default=0)
