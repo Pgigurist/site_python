563466b6e4c59ed57d6728849a454b0746889a42
aho25 <mineevsasha@gmail.com>
2019-03-27 00:18:52 +0300

    adminka in models

--- web_site/registration/admin.py
+++ web_site/registration/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-
+from .models import *
 from .models import MasterClass, Entry
 # Register your models here.
 
 class MasterClassAdmin(admin.ModelAdmin):
     list_display = [field.name for field in MasterClass._meta.fields]
    # listDisplay = ('name', 'date_start', 'date_end')
+    exclude = ["id"]
     inLine = [Entry]
+    search_fields = ['name', 'date_start', 'date_end'] ###subject add
+    list_filter = ["date_start", 'date_end']#subject
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Entry._meta.fields]
+   # listDisplay = ('name', 'date_start', 'date_end')
+    inLine = [Entry]
+    search_fields = ['master_class_id', 'user_id'] ###group number add
+    #list_filter = ["date_start", 'date_end']#grouip number
+
 
 admin.site.register(MasterClass, MasterClassAdmin)
 admin.site.register(Entry)
--- web_site/registration/models.py
+++ web_site/registration/models.py
@@ -1,17 +1,36 @@
+from datetime import date
+
 from django.db import models
-from django.contrib import admin
+
+
 # Create your models here.
 
 class MasterClass(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    #date_start = models.DateTimeField()
-    #date_end = models.DateTimeField()
+    date_start = models.DateTimeField(default=date.today)
+    subject = models.Case()
+    # availiable_seats = models.Count(default=50) ####после регистрации каждого участика будет -1
+    date_end = models.DateTimeField(default=date.today)
 
-    def __str__(self):
-        return self.name
+
+class Meta:
+    verbose_name = "Master Class"
+    verbose_name_plural = "Master Classes"
+
+
+def __str__(self):
+    return self.name
+
 
 class Entry(models.Model):
     user_id = models.PositiveIntegerField(default=0)
     master_class_id = models.ForeignKey(MasterClass, models.CASCADE)
-    verbose_name_plural = "Master Classes"
+   # group_id = models.PositiveIntegerField(default=0)
+    verbose_name = "Entry"
+    verbose_name_plural = "Entries"
+
+
+class Meta:
+    verbose_name = "Entry"
+    verbose_name_plural = "Entries"
